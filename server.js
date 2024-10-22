cdca0741ef763f2cebf3243c93012facbfcd54dc
eunahPae <eunahp86@gmail.com>
2024-10-22 16:20:04 +0900

    mongoDB connect

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
-})
+const { MongoClient } = require('mongodb')
+let db;
+const url = 'mongodb+srv://admin:qwer1234@cluster0.ofyar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+new MongoClient(url).connect().then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('Nodejs')
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+  }).catch((err)=>{
+    console.log(err)
+  })
+
+
 
 // 간단한 서버 코드
 // 아래 경로 접속시, 응답메시지 반환
@@ -15,4 +26,14 @@ app.get('/', (요청, 응답) => {
 
 app.get('/shop', (요청, 응답) => {
     응답.send('쇼핑페이지임~~')
+})
+
+// app.get('/news', (요청, 응답) => {
+//     db.collection('post').insertOne({title : '어쩌구'})
+// })
+
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+    응답.send(result[0].title)
 })
